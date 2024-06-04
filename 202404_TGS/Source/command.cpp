9d45d88c26ecfba5657a41b6e6a8fa96c7b3c4b7
Ibuki Okusada <221038@st.yoshida-g.ac.jp>
2024-06-04 11:40:47 +0900

    パッド用コマンドクラス作成

--- 202404_TGS/Source/command.cpp
+++ 202404_TGS/Source/command.cpp
@@ -1,7 +1,78 @@
 //=============================================================================
 //
-// キーコンフィグ処理 [keyconfig.cpp]
+// コマンド処理 [command.cpp]
 // Author : Ibuki Okusada
 //
 //=============================================================================
 #include "command.h"
+#include "input_gamepad.h"
+
+//=============================================================================
+// コンストラクタ
+//=============================================================================
+CCommand::CCommand()
+{
+	// 値のクリア
+	m_nNowFlame = 0;
+	m_nMaxFlame = 0;
+	m_Info.clear();
+}
+
+//=============================================================================
+// 終了処理
+//=============================================================================
+void CCommand::Uninit()
+{
+	m_Info.clear();
+}
+
+//=============================================================================
+// コマンド設定
+//=============================================================================
+void CCommand::SetCommand(int nKey)
+{
+	m_Info[nKey] = false;	// 登録
+}
+
+//=============================================================================
+// 
+// パッド設定
+// 
+//=============================================================================
+// コマンド入力確認
+//=============================================================================
+bool CCommandPad::GetCommand()
+{
+	bool bValue = false;
+	auto it = m_Info.begin();
+
+	if (it->second) SetNowFlame(GetNowFlame() + 1);	// 一つ目が入力されていたらカウント増加
+
+	for (it; it != m_Info.end(); it++) {
+
+	}
+
+	return bValue;
+}
+
+//=============================================================================
+// 入力方向取得
+//=============================================================================
+CCommandPad::DIRECTION CCommandPad::GetDirection()
+{
+	CInputGamepad* pPad = CInputGamepad::GetInstance();
+	float rot = pPad->GetStickRotR(0);
+	float size = D3DX_PI * 0.125f;
+
+	// 向きから割り出す
+	if		(rot <= 0.0f + size				&& rot >= 0.0f - size)				return DIRECTION::STICK_UP;
+	else if (rot <= -D3DX_PI + size			&& rot >= D3DX_PI - size)			return DIRECTION::STICK_DOWN;
+	else if (rot <= -D3DX_PI * 0.5f + size	&& rot >= -D3DX_PI * 0.5f - size)	return DIRECTION::STICK_LEFT;
+	else if (rot <= D3DX_PI * 0.5f + size	&& rot >= D3DX_PI * 0.5f - size)	return DIRECTION::STICK_RIGHT;
+	else if (rot <= -D3DX_PI * 0.25f + size && rot >= -D3DX_PI * 0.25f - size)	return DIRECTION::STICK_LEFTUP;
+	else if (rot <= D3DX_PI * 0.25f + size	&& rot >= D3DX_PI * 0.25f - size)	return DIRECTION::STICK_RIGHTUP;
+	else if (rot <= -D3DX_PI * 0.75f + size && rot >= -D3DX_PI * 0.75f - size)	return DIRECTION::STICK_LEFTDOWN;
+	else if (rot <= D3DX_PI * 0.75f + size	&& rot >= D3DX_PI * 0.75f - size)	return DIRECTION::STICK_RIGHTDOWN;
+
+	return DIRECTION::STICK_NONE;	// 入力されていない場合
+}
--- 202404_TGS/Source/command.h
+++ 202404_TGS/Source/command.h
@@ -15,14 +15,69 @@ class CCommand
 {
 public:
 
-	CCommand() {}	// コンストラクタ
-	~CCommand() {}	// デストラクタ
+	CCommand();
+	virtual ~CCommand() {}	// デストラクタ
 
-	void Uninit();
+	// 基本処理
+	void Uninit();	// 終了処理
+	virtual bool GetCommand() = 0;	// 入力確認
+
+protected:
+
+	// メンバ関数
+	void SetCommand(int nKey);	// コマンドキー設定
+
+	// 取得系メンバ関数
+	int GetNowFlame() { return m_nNowFlame; }	// 現在のフレーム取得
+	int GetMaxFlame() { return m_nMaxFlame; }	// 最大フレーム取得
+
+	// 設定系メンバ関数
+	void SetNowFlame(int nFlame) { m_nNowFlame = nFlame; }	// 現在のフレーム設定	
+	void SetMaxFlame(int nFlame) { m_nMaxFlame = nFlame; }	// 最大フレーム設定
+
+	// メンバ変数
+	std::map<int, bool> m_Info;	// 情報
 
 private:
 
+	// メンバ変数
+	int m_nNowFlame;	// 現在のフレーム
+	int m_nMaxFlame;	// 入力受付最大フレーム
+};
 
+//==========================================================================
+// 派生クラス定義
+//==========================================================================
+// ゲームパッド
+class CCommandPad : public CCommand
+{
+private:
+
+	// 入力方向列挙
+	enum DIRECTION
+	{
+		STICK_NONE = 0,		// 押してない
+		STICK_UP,			// 上
+		STICK_DOWN,			// 下
+		STICK_LEFT,			// 左
+		STICK_RIGHT,		// 右
+		STICK_LEFTUP,		// 左上	
+		STICK_RIGHTUP,		// 右上
+		STICK_LEFTDOWN,		// 左下
+		STICK_RIGHTDOWN,	// 右下
+		STICK_MAX
+	};
+
+public:
+
+	CCommandPad() {}	// コンストラクタ
+	~CCommandPad() override {}	// デストラクタ
+
+	// メンバ関数
+	bool GetCommand() override;	// 入力確認
+	DIRECTION GetDirection();	// 入力方向取得
+
+private:
 };
 
 #endif
