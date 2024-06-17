715b66ff0ebe44be2d086be7b947e71390faa81d
Soma Ishihara <221008@st.yoshida-g.ac.jp>
2024-06-17 12:09:21 +0900

    判定仮表示

--- 202404_TGS/Source/judgezoneManager.cpp
+++ 202404_TGS/Source/judgezoneManager.cpp
@@ -7,13 +7,22 @@
 #include "judgezoneManager.h"
 #include "judgezone.h"
 #include "judge.h"
+#include "texture.h"
+#include "object2D.h"
+#include <map>
 
 //==========================================================================
 // 定数定義
 //==========================================================================
 namespace
 {
-	
+	std::map<CJudge::JUDGE, std::string> TEXTURE =
+	{
+		{CJudge::JUDGE::JUDGE_AAA,"data\\TEXTURE\\judge_test_01.png"},
+		{CJudge::JUDGE::JUDGE_BBB,"data\\TEXTURE\\judge_test_02.png"},
+		{CJudge::JUDGE::JUDGE_CCC,"data\\TEXTURE\\judge_test_03.png"},
+		{CJudge::JUDGE::JUDGE_DDD,"data\\TEXTURE\\judge_test_04.png"},
+	};
 }
 CJudgeZoneManager* CJudgeZoneManager::m_ThisPtr = nullptr;
 
@@ -88,11 +97,18 @@ void CJudgeZoneManager::Check(float progress)
 			else if (progress > zone.end)
 			{//終了（判定）
 				CJudge::JUDGE judge = (*itr)->Judge();	//ここに判定が入ってる
+				CObject2D* pObj = CObject2D::Create();
+				pObj->SetSize(D3DXVECTOR2(128.0f, 36.0f));
+				pObj->SetPosition(MyLib::Vector3(400.0f, 100.0f, 0.0f));
+				int nTexIdx = CTexture::GetInstance()->Regist(TEXTURE[judge]);
+				pObj->BindTexture(nTexIdx);
+				pObj->SetVtx();
+
 				(*itr)->Uninit();
 			}
 		}
 	}
-
+	
 	// 無効化したゾーン破棄
 	Release();
 }
