d8212abe0bb542ecb516da532359a183c31d51aa
SeigaSouma <221120@st.yoshida-g.ac.jp>
2024-08-27 11:23:59 +0900

    gayaの音

--- 202404_TGS/Source/sound.cpp
+++ 202404_TGS/Source/sound.cpp
@@ -41,7 +41,7 @@ CSound::SOUNDINFO CSound::m_aSoundInfo[LABEL_MAX] =
 	{ "data/SE/receive.wav", 0 },
 	{ "data/SE/oboreru.wav", 0 },
 	{ "data/SE/habataku.wav", 0 },
-	{ "data/SE/gaya.wav", 0 },
+	{ "data/SE/gaya.wav", -1 },
 	{ "data/SE/countdown00.wav", 0 },
 	{ "data/SE/countdown01.wav", 0 },
 	{ "data/SE/start.wav", 0 },
