866b54d4f4e98e8464784315c58e7f34c9fa6ca3
SeigaSouma <221120@st.yoshida-g.ac.jp>
2024-09-17 16:49:11 +0900

    ロード画面とちゅう

--- 202404_TGS/Source/loadscreen.cpp
+++ 202404_TGS/Source/loadscreen.cpp
@@ -29,8 +29,11 @@ namespace
 		"data\\TEXTURE\\load\\mini_n.png",
 		"data\\TEXTURE\\load\\g.png",
 	};
+	const std::string TEXTURE_CYLINDER = "data\\TEXTURE\\load\\cylinder.png";
 	const MyLib::Vector3 STR_DEFPOS = MyLib::Vector3(100.0f, 300.0f, 0.0f);	// 基点の位置
+	const MyLib::Vector3 DEFAULTPOS_CYLINDER = MyLib::Vector3(-300.0f, 600.0f, 0.0f);	// 基点の位置
 	const float STR_HEIGHT = 100.0f;	// 文字列の高さ
+	const float CYLINDER_HEIGHT = 100.0f;	// 筒の高さ
 }
 
 
@@ -48,6 +51,10 @@ CLoadScreen::CLoadScreen()
 	{
 		m_apObj2D[i] = nullptr;
 	}
+
+	// 筒
+	m_pCylinder = nullptr;
+	m_fBobbingTime = 0.0f;		// ぷかぷかタイマー
 }
 
 //==========================================================================
@@ -61,31 +68,22 @@ CLoadScreen::~CLoadScreen()
 //==========================================================================
 // 生成処理
 //==========================================================================
-CLoadScreen *CLoadScreen::Create()
+CLoadScreen* CLoadScreen::Create()
 {
-	// 生成用のオブジェクト
-	CLoadScreen *pFade = nullptr;
+	// メモリの確保
+	CLoadScreen* pFade = DEBUG_NEW CLoadScreen;
 
-	if (pFade == nullptr)
-	{// nullptrだったら
+	if (pFade != nullptr)
+	{// メモリの確保が出来ていたら
 
-		// メモリの確保
-		pFade = DEBUG_NEW CLoadScreen;
-
-		if (pFade != nullptr)
-		{// メモリの確保が出来ていたら
-
-			// 初期化処理
-			if (FAILED(pFade->Init()))
-			{// 失敗していたら
-				return nullptr;
-			}
+		// 初期化処理
+		if (FAILED(pFade->Init()))
+		{// 失敗していたら
+			return nullptr;
 		}
-
-		return pFade;
 	}
 
-	return nullptr;
+	return pFade;
 }
 
 //==========================================================================
@@ -121,7 +119,31 @@ HRESULT CLoadScreen::Init()
 		}
 	}
 
+	// 筒生成
+	CreateCylinder();
+
 	return S_OK;
+}
+
+//==========================================================================
+// 筒生成
+//==========================================================================
+void CLoadScreen::CreateCylinder()
+{
+	// 筒生成
+	m_pCylinder = CObject2D::Create();
+	m_pCylinder->SetType(CObject::TYPE::TYPE_NONE);
+	m_pCylinder->SetPosition(DEFAULTPOS_CYLINDER);
+	m_pCylinder->SetOriginPosition(DEFAULTPOS_CYLINDER);
+
+	// テクスチャ割り当て
+	int nIdx = CTexture::GetInstance()->Regist(TEXTURE_CYLINDER);
+	m_pCylinder->BindTexture(nIdx);
+
+	// サイズ変更
+	D3DXVECTOR2 size = UtilFunc::Transformation::AdjustSizeByHeight(CTexture::GetInstance()->GetImageSize(nIdx), CYLINDER_HEIGHT);
+	m_pCylinder->SetSize(size);
+
 }
 
 //==========================================================================
@@ -138,8 +160,17 @@ void CLoadScreen::Uninit()
 			m_apObj2D[i] = nullptr;
 		}
 	}
+
+	if (m_pCylinder != nullptr)
+	{
+		m_pCylinder->Uninit();
+		m_pCylinder = nullptr;
+	}
 }
 
+//==========================================================================
+// 削除処理
+//==========================================================================
 void CLoadScreen::Kill()
 {
 	for (int i = 0; i < NUM_STRING; i++)
@@ -150,6 +181,13 @@ void CLoadScreen::Kill()
 			delete m_apObj2D[i];
 			m_apObj2D[i] = nullptr;
 		}
+	}
+
+	if (m_pCylinder != nullptr)
+	{
+		m_pCylinder->Uninit();
+		delete m_pCylinder;
+		m_pCylinder = nullptr;
 	}
 }
 
@@ -167,6 +205,50 @@ void CLoadScreen::Update()
 		}
 	}
 
+	// 筒の動き
+	MoveCylinder();
+	if (m_pCylinder != nullptr)
+	{
+		m_pCylinder->Update();
+	}
+}
+
+//==========================================================================
+// 文字との判定
+//==========================================================================
+void CLoadScreen::CollisionText()
+{
+	if (m_pCylinder == nullptr) return;
+
+	// 位置取得
+	MyLib::Vector3 pos = m_pCylinder->GetPosition();
+}
+
+//==========================================================================
+// 筒の動き
+//==========================================================================
+void CLoadScreen::MoveCylinder()
+{
+	if (m_pCylinder == nullptr) return;
+
+	// 位置取得
+	MyLib::Vector3 pos = m_pCylinder->GetPosition(), posOrigin = m_pCylinder->GetOriginPosition();
+
+	// 横移動
+	pos.x += 5.0f;
+
+	// ぷかぷか
+	m_fBobbingTime += CManager::GetInstance()->GetDeltaTime();
+	pos.y = posOrigin.y + sinf(D3DX_PI * (m_fBobbingTime / 0.8f)) * 10.0f;
+
+	// 折り返し
+	if (pos.x >= 1280.0f + m_pCylinder->GetSize().x)
+	{
+		pos.x = -100.0f;
+	}
+
+	// 位置設定
+	m_pCylinder->SetPosition(pos);
 }
 
 //==========================================================================
@@ -174,12 +256,17 @@ void CLoadScreen::Update()
 //==========================================================================
 void CLoadScreen::Draw()
 {
-	// 文字生成
+	// 文字描画
 	for (int i = 0; i < NUM_STRING; i++)
 	{
 		if (m_apObj2D[i] != nullptr)
 		{
 			m_apObj2D[i]->Draw();
 		}
+	}
+
+	if (m_pCylinder != nullptr)
+	{
+		m_pCylinder->Draw();
 	}
 }
